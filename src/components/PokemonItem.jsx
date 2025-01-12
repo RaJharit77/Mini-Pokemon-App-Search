ef99a40658d275752fffd62ca4e0a62346aba6d4
RaJharit77 <hei.rajo.2@gmail.com>
2025-01-13 00:14:53 +0300

    feat: Update PokemonItem page

--- src/components/PokemonItem.jsx
+++ src/components/PokemonItem.jsx
@@ -31,7 +31,7 @@ const PokemonItem = () => {
                     <h2 className="text-center text-3xl font-bold capitalize text-white">
                         {pokemon.name}
                     </h2>
-                    <p className="text-center text-xl text-white"># {pokemon.id}</p>
+                    <p className="text-center text-xl text-white">#{pokemon.id}</p>
                     <img
                         src={pokemon.sprites?.other?.['official-artwork']?.front_default || pokemon.sprites?.front_default}
                         alt={pokemon.name}
@@ -43,7 +43,7 @@ const PokemonItem = () => {
             )}
             <button
                 onClick={() => navigate(-1)}
-                className="mt-6 px-4 py-2 bg-orange-500 text-white rounded-md hover:bg-orange-600 block mx-auto"
+                className="mt-6 px-4 py-2 bg-orange-500 text-white hover:text-black rounded-md hover:bg-orange-600 block mx-auto"
             >
                 <GiReturnArrow />
             </button>

095871a5ccc05898f15c14b4a5ba3304588391c4
RaJharit77 <hei.rajo.2@gmail.com>
2025-01-12 22:25:38 +0300

    feat: Update PokemonSearchDumb page

--- src/components/PokemonSearchDumb.jsx
+++ src/components/PokemonSearchDumb.jsx
@@ -8,12 +8,12 @@ const PokemonSearchDumb = ({ searchRef, onSearch, onRandom }) => {
                 type="text"
                 ref={searchRef}
                 placeholder="Entrez un nom ou un ID"
-                className="mt-1 block border pl-5 pr-16 py-3 rounded-md w-full max-w-lg focus:outline-none text-lg text-leftm h-12 bg-black text-gray-100"
+                className="mt-1 block border pl-5 pr-16 py-3 rounded-md w-full max-w-lg focus:outline-none text-lg text-leftm h-12 bg-gray-950 text-gray-100"
             />
             <div className="flex gap-3">
                 <button
                     onClick={onSearch}
-                    className="px-7 py-3 bg-yellow-400 text-black hover:text-white rounded-md hover:bg-yellow-500"
+                    className="px-7 py-3 bg-yellow-400 text-black hover:text-black rounded-md hover:bg-yellow-500"
                 >
                     <FaSearch />
                 </button>
